929ed59ec8d0820df5335c6a25c4e19204d1b8dd
bhadresh gosai <45452903+bhadresh-goswami@users.noreply.github.com>
2026-04-10 04:28:14 +0530

    base url change

--- ui/src/api/client.ts
+++ ui/src/api/client.ts
@@ -1,7 +1,7 @@
 let unauthorizedHandler: (() => void) | undefined
 
 const AUTH_STORAGE_KEY = 'crm_auth'
-const API_BASE_URL = '/api'
+const API_BASE_URL = 'https://support.bsquareg-developers.com/api'
 
 const isAbsoluteUrl = (path: string) => path.startsWith('http://') || path.startsWith('https://')
 
