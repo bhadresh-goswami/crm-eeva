56975e6bffbd39dfaa2d344df76ef0b33fcc00b8
bhadresh gosai <45452903+bhadresh-goswami@users.noreply.github.com>
2026-05-05 23:10:50 +0530

    commit

--- ui/src/routes/AppRoutes.tsx
+++ ui/src/routes/AppRoutes.tsx
@@ -12,10 +12,9 @@ import BulkPriceUpdatePage from '../modules/tasks/pages/BulkPriceUpdatePage'
 import ReportsTasksPage from '../modules/tasks/pages/ReportsTasksPage'
 import CandidateReportPage from '../modules/tasks/pages/CandidateReportPage'
 import ExpertTaskReportsPage from '../modules/tasks/pages/ExpertTaskReportsPage'
-<<<<<<< codex/create-api-to-fetch-expert-tasks-bwxbgx
 import FeedbackReportPage from '../modules/tasks/pages/FeedbackReportPage'
-=======
->>>>>>> sprint-4-feedback-system
+
+
 import UsersPage from '../modules/users/pages/UsersPage'
 import InvoiceListPage from '../modules/invoices/pages/InvoiceListPage'
 import InvoiceCreatePage from '../modules/invoices/pages/InvoiceCreatePage'
